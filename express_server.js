c1089a32582698e653ceac69d6bf9890a8c2cfd1
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-03-26 17:34:14 +0000

    created route for urls_index

--- express_server.js
+++ express_server.js
@@ -17,8 +17,9 @@ app.get('/urls.json', (req, res) => {
     res.json(urlDatabase)
 });
 
-app.get('/hello', (req, res) => {
-    res.send("<html><body>Hello <b>World</b></body></html>\n")
+app.get('/urls', (req, res) => {
+    let templateVars = { urls: urlDatabase }
+    res.render('urls_index', templateVars);
 });
 
 app.listen(PORT, () => {
