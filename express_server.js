f7dd1659e34da74ceda0cc9ed59f89c496411c23
Ryan Andrew Sibat <ryanandrew.sibat@gmail.com>
2019-03-26 19:04:53 +0000

    created post route for getting longURLs

--- express_server.js
+++ express_server.js
@@ -1,14 +1,29 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const PORT = 8080;
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
     "9sm5xK": "http://www.google.com"
 };
 
+// <-- HELPER FUNCTIONS -->
+
+function generateRandomString() {
+    let text = "";
+    const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (let i = 0; i < 6; i++){
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+}
+
+// <-- GET -->
 app.get('/', (req, res) => {
     res.send('Hello!');
 });
@@ -22,9 +37,18 @@ app.get('/urls', (req, res) => {
     res.render('urls_index', templateVars);
 });
 
+app.get('/urls/new', (req, res) => {
+    res.render('urls_new');
+});
+
 app.get('/urls/:shortURL', (req, res) => {
     const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] }
     res.render('urls_show', templateVars);
+});
+
+// <-- POST -->
+app.post("/urls", (req, res) => {
+    res.send('okay')
 });
 
 app.listen(PORT, () => {
